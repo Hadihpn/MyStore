eab298becccf070d01b9e8bf25517b9a9b2ae7db
unknown <persnb@gmail.com>
2024-03-26 00:24:17 +0330

    complete add Category function

--- src/common/validators/admin/category.schema.js
+++ src/common/validators/admin/category.schema.js
@@ -3,6 +3,7 @@ const { MongoIDPattern } = require("../../constant/constantVar");
 const addCategorySchema = Joi.object({
     title:Joi.string().min(3).max(20).error(new Error("عنوان  وارد شده معتبر نمی باشد")),
     slug:Joi.string().min(3).max(20).error(new Error("اسلاگ  وارد شده معتبر نمی باشد")),
+    icon:Joi.string().min(3).max(20).error(new Error("ایکون  وارد شده معتبر نمی باشد")),
     parent:Joi.string().allow('').pattern(MongoIDPattern).allow('').error(new Error("ادرس دسته بندی پدر معتبر نمی باشد")),
     
 })
