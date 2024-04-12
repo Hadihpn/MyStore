8b7fc1b93266a560642ffcdc0bafbc15c9ebe57b
unknown <persnb@gmail.com>
2024-04-12 22:06:41 +0330

    add product swagger definition

--- src/modules/admin/product/product.swagger.js
+++ src/modules/admin/product/product.swagger.js
@@ -10,6 +10,52 @@
  */ 
 /**
  * @swagger
+ *  definitions:
+ *      ListOfCourses:
+ *          type: object
+ *          properties:
+ *              statusCode: 
+ *                  type: integer
+ *                  example: 200
+ *              data:
+ *                  type: object
+ *                  properties:
+ *                      courses:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              properties:
+ *                                  _id:
+ *                                      type: string
+ *                                      example: "62822e4ff68cdded54aa928d"
+ *                                  title:
+ *                                      type: string
+ *                                      example: "title of course"
+ *                                  short_text:
+ *                                      type: string
+ *                                      example: "summary text of course"
+ *                                  text:
+ *                                      type: string
+ *                                      example: "text and describe of course"
+ *                                  time:
+ *                                      type: string
+ *                                      example: "01:22:34"
+ *                                  price:
+ *                                      type: integer
+ *                                      example: 250,000
+ *                                  discount:
+ *                                      type: interger
+ *                                      example: 20
+ *                                  studendtCount:
+ *                                      type: integer
+ *                                      example: 340
+ *                                  teacher:
+ *                                      type: string
+ *                                      example: "erfan yousefi"
+ */
+
+/**
+ * @swagger
  *  components:
  *      schemas:
  *          addProduct:
