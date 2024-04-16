7514992d1bf9508fe97387aba7c2fb1550852614
unknown <persnb@gmail.com>
2024-04-16 22:59:11 +0330

    add populate in show course list

--- src/modules/admin/course/course.model.js
+++ src/modules/admin/course/course.model.js
@@ -17,7 +17,7 @@ const CourseSchema = new mongoose.Schema({
     text: { type: String, required: true },
     image: { type: String, required: true },
     tags: { type: [String], required: true },
-    category: { type: mongoose.Types.ObjectId, reg: "category", required: true },
+    category: { type: mongoose.Types.ObjectId, ref: "Category", required: true },
     comments: { type: [CommentSchema], default: [] },
     likes: { type: [mongoose.Types.ObjectId], default: [] },
     dislikes: { type: [mongoose.Types.ObjectId], default: [] },
@@ -26,10 +26,17 @@ const CourseSchema = new mongoose.Schema({
     discount: { type: Number, default: 0 },
     type: { type: String, default: "free", required: true },//رایگان یا پولی
     time: { type: String, default: "00:00:00" },
-    teacher: { type: mongoose.Types.ObjectId, required: true },
+    teacher: { type: mongoose.Types.ObjectId, ref: "user", required: true },
     chapters: { type: [Chapter], default: [] },
     students: { type: [Types.ObjectId], default: [], ref: "user" }
 })
+
+function autoPopulate(next) {
+    this.populate([{ path: "teacher", select: { phone: 1 } }
+    , { path: "category",select:{ title:1} }])
+    next()
+}
+CourseSchema.pre("find", autoPopulate).pre("findOne", autoPopulate)
 CourseSchema.index({ title: "text", short_text: "text" })
 const CourseModel = model("Course", CourseSchema);
 module.exports = { CourseModel };
