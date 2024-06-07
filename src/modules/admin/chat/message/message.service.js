752c7b32522d514e65dcaa76b0ca56cc30b25852
unknown <persnb@gmail.com>
2024-06-07 17:34:51 +0330

    add room & message Based

--- src/modules/admin/chat/message/message.service.js
+++
@@ -1,9 +0,0 @@
-const autoBind = require("auto-bind")
-
-class MessageService{
-constructor(){
-    autoBind(this);
-}
-}
-
-module.exports= new MessageService()
--- src/modules/admin/chat/namespace/namespace.routes.js
+++ src/modules/admin/chat/namespace/namespace.routes.js
@@ -1,9 +1,23 @@
 const namespaceController = require("./namespace.controller");
 
 const router = require("express").Router();
+//#region Namespace
 
-router.post("/add",namespaceController.addNamespace)
-router.get("/",namespaceController.getNamespaces)
+router.post("/add", namespaceController.addNamespace)
+router.get("/", namespaceController.getNamespaces)
+
+//#endregion
+//#region Room
+router.post("/room/add", namespaceController.addNamespace)
+
+//#endregion
+
+//#region Message
+router.post("/message/add", namespaceController.addNamespace)
+
+//#endregion
+
+
 module.exports = {
-    NameSpaceRoutes:router
+    NameSpaceRoutes: router
 }
