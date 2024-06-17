e2e65a8c323d1c3ef0a17c98a238573dd1d4795e
unknown <persnb@gmail.com>
2024-06-17 13:28:42 +0330

    get rooms of firs(default) namespace

--- src/socket.io/namespaces.socket.js
+++ src/socket.io/namespaces.socket.js
@@ -12,30 +12,32 @@ class NameSpaceSocketHandler {
             socket.emit("namespacesList", namespacesList);
         })
     }
-    // async createNamespacesConnection() {
-    //     const namespacesList = await namespaceService.getNameSpaces()
-    //     for (const namespace of namespacesList) {
-    //         this.#io.of(`/${namespace.endPoint}`).on("connection", async socket => {
-    //             socket.emit("roomList", namespace.rooms)
-    //             socket.on("JoinRoom", async roomName => {
-    //                 const lastRoom = Array.from(socket.rooms)[1]
-    //                 if (lastRoom) {
-    //                     socket.leave(lastRoom)
-    //                 }
-    //                 socket.join(roomName)
-    //                 // console.log(namespace.endPoint, roomName);
-    //                 await this.getCountOfOnlineUser(namespace.endPoint, roomName)
-    //                 const roomInfo = await namespaceService.getRoomWithName(roomName)
-    //                 socket.emit("roomInfo", roomInfo)
-    //             })
-    //         })
-    //     }
-    // }
-    // async getCountOfOnlineUser(endPoint, roomName) {
-    //     const onlineUser = await this.#io.of(`/${endPoint}`).in(roomName).allSockets();
-    //     // console.log(`onlineUsers: ${Array.from(onlineUser).length}`);
-    //     this.#io.of(`/${endPoint}`).in(roomName).emit("OnlineUserCount", Array.from(onlineUser).length)
+    async createNamespacesConnection() {
+        const namespacesList = await namespaceService.getNameSpaces()
+        this.#io.of(`/${namespacesList[0].endPoint}`).on("connection", async socket => {
+            socket.emit("roomList", namespacesList[0].rooms)})
+        // for (const namespace of namespacesList) {
+        //     this.#io.of(`/${namespace.endPoint}`).on("connection", async socket => {
+        //         socket.emit("roomList", namespace.rooms)
+        //         socket.on("JoinRoom", async roomName => {
+        //             const lastRoom = Array.from(socket.rooms)[1]
+        //             if (lastRoom) {
+        //                 socket.leave(lastRoom)
+        //             }
+        //             socket.join(roomName)
+        //             // console.log(namespace.endPoint, roomName);
+        //             await this.getCountOfOnlineUser(namespace.endPoint, roomName)
+        //             const roomInfo = await namespaceService.getRoomWithName(roomName)
+        //             socket.emit("roomInfo", roomInfo)
+        //         })
+        //     })
+        // }
+    }
+    async getCountOfOnlineUser(endPoint, roomName) {
+        const onlineUser = await this.#io.of(`/${endPoint}`).in(roomName).allSockets();
+        // console.log(`onlineUsers: ${Array.from(onlineUser).length}`);
+        this.#io.of(`/${endPoint}`).in(roomName).emit("OnlineUserCount", Array.from(onlineUser).length)
 
-    // }
+    }
 }
 module.exports = NameSpaceSocketHandler
